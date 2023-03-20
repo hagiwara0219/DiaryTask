4df95fb203c7c4769bfc2a158770e03e4d56796d
17hagiwara <17hagiwara@gmail.com>
2023-03-20 12:56:22 +0900

    320 task

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-console.assert('こんにはこれはタスクです。');
-console.log('3/17(fri)task setting ok');
-console.log('git push test');
+// alert('これはタスクです。');
+// prompt('これはテキスト入力のpromptです');
+if (confirm("wanna you check task?")) {
+    alert("閲覧をどうぞ");
+} else {
+    alert("googleに戻ります");
+    location.href = "https://www.google.com/";
+}
